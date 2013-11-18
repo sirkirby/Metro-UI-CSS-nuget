bf35b28ec8f1f9e9d9484efcd3ff2660b101451a
Chris Kirby <chris@infinitusdev.com>
2013-11-18 13:38:05 -0800

    remodeled package structure and source update
    updated all source to v2.0.1. updated package structure to accommodate
    the new source as well as some other functional updates. modified the
    bundle code to correctly allow for nesting. package set to b1 with
    updated links. added mit license to repo and updated readme.

--- MetroUICSSBundleConfig.cs.pp
+++ MetroUICSSBundleConfig.cs.pp
@@ -8,11 +8,11 @@ namespace $rootnamespace$.App_Start
 	{
 		public static void RegisterBundles()
 		{
-			// Add @Styles.Render("~/Content/metroui") in the <head/> of your _Layout.cshtml view
-			// Add @Scripts.Render("~/bundles/metroui") after jQuery in your _Layout.cshtml view
+			// Add @Styles.Render("~/Content/metro-ui/css") in the <head/> of your _Layout.cshtml view
+			// Add @Scripts.Render("~/bundles/metro-ui") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, ASP.Net will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/metroui").Include("~/Scripts/metroui/accordion.js", "~/Scripts/metroui/buttonset.js", "~/Scripts/metroui/calendar.js", "~/Scripts/metroui/carousel.js", "~/Scripts/metroui/dialog.js", "~/Scripts/metroui/dropdown.js", "~/Scripts/metroui/input-control.js", "~/Scripts/metroui/pagecontrol.js", "~/Scripts/metroui/rating.js", "~/Scripts/metroui/slider.js", "~/Scripts/metroui/start-menu.js", "~/Scripts/metroui/tile-drag.js", "~/Scripts/metroui/tile-slider.js"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/metroui").Include("~/Content/metroui/css/modern.css", "~/Content/metroui/css/modern-responsive.css"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/metro-ui").Include("~/Scripts/metro-ui/metro-*"));
+			BundleTable.Bundles.Add(new StyleBundle("~/Content/metro-ui/css").Include("~/Content/metro-ui/css/metro-bootstrap.css", "~/Content/metro-ui/css/metro-bootstrap-responsive.css"));
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Metro-UI-CSS-nuget
+Metro UI CSS
 ==================
 
-nuget packages for the metro ui css front end framework
+Nuget packages for the Metro UI CSS front end framework. 
+
+For more details on the project, visit them here http://metroui.org.ua/. If you prefer the full source nuget package, including the less files, check out https://www.nuget.org/packages/metro.ui.css.
+
+[License](LICENSE.txt)
+==================

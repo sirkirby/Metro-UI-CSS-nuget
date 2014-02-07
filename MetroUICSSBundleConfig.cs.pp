24b90b790b64a84345ec4ac47accaa791e5f05d1
Chris Kirby <chris@infinitusdev.com>
2014-02-07 11:17:16 -0800

    updated to v2.0.23 source. removed all js plugins in favor of compiled minified version. added in min versions of css. updated all dependencies.

--- MetroUICSSBundleConfig.cs.pp
+++ MetroUICSSBundleConfig.cs.pp
@@ -11,7 +11,7 @@ namespace $rootnamespace$.App_Start
 			// Add @Styles.Render("~/Content/metro-ui/css") in the <head/> of your _Layout.cshtml view
 			// Add @Scripts.Render("~/bundles/metro-ui") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, ASP.Net will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/metro-ui").Include("~/Scripts/metro-ui/jquery.ui.widget.js").Include("~/Scripts/metro-ui/metro-*"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/metro-ui").Include("~/Scripts/metro-ui/jquery.ui.widget.js").Include("~/Scripts/metro-ui/metro.min.js"));
 			BundleTable.Bundles.Add(new StyleBundle("~/Content/metro-ui/css").Include("~/Content/metro-ui/css/metro-bootstrap.css", "~/Content/metro-ui/css/metro-bootstrap-responsive.css"));
 		}
 	}
